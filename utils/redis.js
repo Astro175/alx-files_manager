042dbad6c9d0f351af4d67506958ea4f7e058ec2
astro175 <aspark400@gmail.com>
2023-09-28 11:38:17 +0100

    fixed errors with redis.js server connection

--- utils/redis.js
+++ utils/redis.js
@@ -17,7 +17,7 @@ class RedisClient {
   }
 
   async get (key) {
-    return new Promise((reolve, reject) => {
+    return new Promise((resolve, reject) => {
       this.client.get(key, (err, reply) => {
         if (err) {
           reject(err);
