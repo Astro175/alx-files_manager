c883aee80e455100f1ed2ffcc23bde4671b85354
astro175 <aspark400@gmail.com>
2023-10-03 15:38:12 +0100

    fixed errors and lint in redis.js

--- utils/db.js
+++ utils/db.js
@@ -1,31 +1,32 @@
-const Collection = require('mongodb/lib/collection');
-
 const { MongoClient } = require('mongodb');
 
 class DBClient {
-  constructor () {
+  constructor() {
     // Use ternary if statement to determine the variables
-    const host = (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost'
+    const host = (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost';
     const port = (process.env.DB_PORT) ? process.env.DB_HOST : 27017;
     this.db = (process.env.DB_DATABASE) ? process.env.DB_DATABASE : 'files_manager';
     const url = `mongodb://${host}:${port}`;
-    this.client = new MongoClient(url);
-
-
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect();
   }
 
-  isAlive () {
-   return this.client.connect()
+  isAlive() {
+    return this.client && this.client.topology.isConnected();
   }
 
-  async nbUsers () {
-    const numUser = await Collection.find({ users }).toArray();
-    return numUser;
+  async nbUsers() {
+    const db = this.client.db(this.db);
+    const collectionUsers = db.collection('users');
+    const numUsers = await collectionUsers.countDocuments({});
+    return numUsers;
   }
 
-  async nbFiles () {
-    const numFiles = await Collection.find({ files }).toArray();
-    return numFiles;
+  async nbFiles() {
+    const db = this.client.db(this.db);
+    const collectionUsers = db.collection('files');
+    const numUsers = await collectionUsers.countDocuments({});
+    return numUsers;
   }
 }
 
