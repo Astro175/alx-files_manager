3dfb3986905ef48dbc9fbecf9bf9cd6c939a7803
astro175 <aspark400@gmail.com>
2023-10-22 13:08:16 +0100

    done task 5

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -3,6 +3,9 @@ import { v4 as uuidv4 } from 'uuid';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
+const { promisify } = require('util');
+const mkdir = promisify(require('fs').mkdir);
+
 const fs = require('fs').promises;
 const path = require('path');
 
@@ -12,17 +15,27 @@ class FilesController {
   static async postUpload(req, res) {
     const token = req.headers['x-token'];
     const key = `auth_${token}`;
-    const value = redisClient.get(key);
+    const value = await redisClient.get(key);
+
     const _id = ObjectId(value);
-    const user = Usercollection.find({ _id });
+    const user = await Usercollection.findOne({ _id });
 
     if (!user) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
     const {
-      name, type, isPublic, data,
+      name, type, data,
     } = req.body;
-    let { parentId } = req.body;
+    let { parentId, isPublic } = req.body;
+    const userId = user._id;
+
+    if (!isPublic) {
+      isPublic = 'false';
+    }
+
+    if (!parentId) {
+      parentId = 0;
+    }
 
     if (!name) {
       return res.status(400).json({ error: 'Missing name' });
@@ -44,7 +57,7 @@ class FilesController {
         return res.status(400).json({ error: 'Parent is not a folder' });
       }
     }
-    const userId = user._id;
+
     if (type === 'folder') {
       const newFile = {
         userId,
@@ -53,13 +66,23 @@ class FilesController {
         parentId,
         isPublic,
       };
-      const folder = await Filecollection.insertOne(newFile);
-      return res.status(201).json({ folder });
+      const results = await Filecollection.insertOne(newFile);
+      const folder = results.ops[0];
+      return res.status(201).send(folder);
     }
     const directoryPath = (process.env.FOLDER_PATH) ? process.env.FOLDER_PATH : '/tmp/files_manager';
     const filename = uuidv4();
     const filepath = path.join(directoryPath, filename);
     const fileData = Buffer.from(data, 'base64').toString('utf-8');
+
+    async function ensuredirexists(dirPath) {
+      try {
+        await mkdir(dirPath, { recursive: true });
+      } catch (err) {
+        console.log(`Error creating the directory: ${err.message}`);
+      }
+    }
+    await ensuredirexists(directoryPath);
     try {
       await fs.writeFile(filepath, fileData);
     } catch (err) {
@@ -67,9 +90,7 @@ class FilesController {
     }
     const localpath = path.join(__dirname, filepath);
 
-    if (!parentId) {
-      parentId = 0;
-    }
+    console.log(userId);
     const newFile = {
       userId,
       name,
@@ -78,8 +99,9 @@ class FilesController {
       isPublic,
       localpath,
     };
-    const folder = await Filecollection.insertOne(newFile);
-    return res.status(201).json({ folder });
+    const result = await Filecollection.insertOne(newFile);
+    const folder = result.ops[0];
+    return res.status(201).send(folder);
   }
 }
 
